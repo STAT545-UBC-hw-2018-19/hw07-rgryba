689dfa44e7ab426d818c19dc02ca0111f77a1b3f
rgryba <r.gryba@stat.ubc.ca>
2018-11-11 14:04:18 -0800

    Commiting edits to data package.

---
+++ argosData/R/ebDoc.R
@@ -0,0 +1,8 @@
+#' Argos satellite data for bearded seals (Erignathus barbatus)
+#'
+#' ??? Fill in
+#'
+#'
+#' @format The data format is a combination if integers, factors and date.
+#' @source The data is a product of Argos satellite tagging in Alaskan waters
+"ebArgos"
