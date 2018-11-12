db1e359f9d1367fb31462cdf747fd35d000a0d06
rgryba <r.gryba@stat.ubc.ca>
2018-11-12 08:32:10 -0900

    Commit of edits to documentation.

--- argosData/R/ebArgos.R
+++ argosData/R/ebArgos.R
@@ -1,7 +1,15 @@
-#' Argos data for bearded seals tagged in Alaskan waters
+#' Argos satellite location data for bearded seals tagged in Alaskan waters
 #'
-#' ??????
+#' A dataset containing the locaitons of bearded seals via Argos satellite and the
+#' locaiton quaility.
 #'
-#' @format Character, integer, factor
-#' @source Project conducted as a part of the BOEM project MARES.
+#' @format A data frame with 1557 rows and 5 variables.
+#' \describe{
+#'     \item{id}{identity number for each seal tagged}
+#'     \item{time}{date and time of the locaiton, in UTM}
+#'     \item{lc}{Argos location quality, 3, 2, 1, 0, A, B, Z}
+#'     \item{lon}{longitude of the seal location}
+#'     \item{lat}{latitude of the seal location}
+#'     }
+#' @source \url{}
 "ebArgos"
